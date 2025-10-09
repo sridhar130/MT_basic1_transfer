b8ed5f16d6c43d555af3d402056a3f67e13ae2e6
SRIDHAR TRIPATHY <tripathysridhar@gmail.com>
2025-10-08 23:17:52 -0500

    Revert "mt and analysis"

---
+++ build/CMakeFiles/MT.dir/DependInfo.cmake
@@ -0,0 +1,53 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/sridhar/test_examples/MT_basic1_transfer/MT.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/MT.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/ActionInitialization.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/ActionInitialization.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/DetectorConstruction.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/DetectorConstruction.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/EventAction.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/EventAction.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/PrimaryGeneratorAction.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/PrimaryGeneratorAction.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/RunAction.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/RunAction.cc.o"
+  "/home/sridhar/test_examples/MT_basic1_transfer/src/SteppingAction.cc" "/home/sridhar/test_examples/MT_basic1_transfer/build/CMakeFiles/MT.dir/src/SteppingAction.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "G4INTY_USE_QT"
+  "G4INTY_USE_XT"
+  "G4LIB_BUILD_DLL"
+  "G4UI_USE_QT"
+  "G4UI_USE_TCSH"
+  "G4VIS_USE_OPENGL"
+  "G4VIS_USE_OPENGLQT"
+  "G4VIS_USE_OPENGLX"
+  "QT_CORE_LIB"
+  "QT_GUI_LIB"
+  "QT_NO_DEBUG"
+  "QT_OPENGL_LIB"
+  "QT_PRINTSUPPORT_LIB"
+  "QT_WIDGETS_LIB"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../include"
+  "/home/sridhar/g4model-wip/geant4.10.07.p01-install/include/Geant4"
+  "/usr/include/x86_64-linux-gnu/qt5"
+  "/usr/include/x86_64-linux-gnu/qt5/QtCore"
+  "/usr/lib/x86_64-linux-gnu/qt5/mkspecs/linux-g++"
+  "/usr/include/x86_64-linux-gnu/qt5/QtGui"
+  "/usr/include/x86_64-linux-gnu/qt5/QtWidgets"
+  "/usr/include/x86_64-linux-gnu/qt5/QtOpenGL"
+  "/usr/include/x86_64-linux-gnu/qt5/QtPrintSupport"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
